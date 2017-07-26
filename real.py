320a497abe95f86ed1399c8e1de980e78976e7b2
muhan <yuanmh@umich.edu>
2017-07-26 00:25:56 -0700

    adding some notes

--- real.py
+++ real.py
@@ -12,12 +12,9 @@ from sklearn.svm import LinearSVC
 import json
 import sys
 reload(sys)
+
 sys.setdefaultencoding('utf-8')
-
 st = LancasterStemmer()
-
-def stemming(s,st):
-    return st.stem(s.decode())
 
 def swear(l,s):
     for j in s:
@@ -32,33 +29,16 @@ def depunc(t):
             t = t.replace(j, " !")
     return t
 
-def destopword(l,stop):
-    for j in stop:
-        l = l.replace(j,"")
-    return l
-
-def stemming_str(s):
-    temp_l = s.split()
-    for i in temp_l:
-        s = s.replace(i , stemming(i,st))
-    return s
-
-
 with open("swear.js","r") as infile:
     sw = json.loads(infile.read())
 
-print len(sw)
-with open("s.txt","r") as inf:
-    for i in inf.readlines():
-        sw.append(i.strip().decode())
-sw = list(set(sw))
+# print len(sw)
+# with open("s.txt","r") as inf:
+#     for i in inf.readlines():
+#         if i.strip().decode() not in sw:
+#             sw.append(i.strip().decode())
 
-with open("stoplist.txt","r") as stopfile:
-    stoplist = stopfile.readlines()
-    stoplist = [i.strip() for i in stoplist]
-
-print len(sw)
-print "###"
+print "Start"
 
 with open("train.tsv","r") as trainfile:
     trainfile.readline()
@@ -66,7 +46,7 @@ with open("train.tsv","r") as trainfile:
     traindatalist = [swear(depunc(i[1]),sw) for i in trainlist]
     trainreslist = [i[0] for i in trainlist]
 
-print "###"
+print "Finished Loading Training Set"
 
 with open("test.tsv","r") as testfile:
     testfile.readline()
@@ -74,29 +54,29 @@ with open("test.tsv","r") as testfile:
     testdatalist = [swear(depunc(i[1]),sw) for i in testlist]
     testIDlist = [i[0] for i in testlist]
 
-print "####"
+print "Finished Loading Testing Set"
 
 pipeline = Pipeline([
     ('vect', TfidfVectorizer()),
     # ('vect', CountVectorizer()),
     # ('tfidf', TfidfTransformer()),
-    ('clf', LinearSVC(class_weight= 'balanced',C=10))
+    ('clf', LinearSVC(class_weight= 'balanced'))
     ])
 
 parameters = {
-    'vect__ngram_range': [(1, 2)]
-    # 'vect__max_df': (0.6),
+    'vect__ngram_range': [(1, 2)],
+    'vect__max_df': [0.6],
     # 'tfidf__use_idf': (True, False),
     # 'tfidf__norm': ('l1', 'l2'),
-    # 'clf__C': [10]
+    'clf__C': [10]
     # 'clf__dual':(True, False)
 }
 
 grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
 grid_search.fit(traindatalist, trainreslist)
-# {'vect__ngram_range': (1, 2), 'clf__C': 1} LinearSVC
-# {'vect__ngram_range': (1, 2), 'clf__C': 10}
-print grid_search.best_params_
+# {'vect__ngram_range': (1, 2), 'clf__C': 10, 'vect__max_df': 0.6}
+
+# print grid_search.best_params_
 outres = grid_search.predict(testdatalist)
 
 
@@ -107,4 +87,5 @@ with open("submission.csv","w") as fout:
         fout.write(",")
         fout.write(outres[int(i)])
         fout.write("\n")
+
 # http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html#sklearn.model_selection.GridSearchCV
